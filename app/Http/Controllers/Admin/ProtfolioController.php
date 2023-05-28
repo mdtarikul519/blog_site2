ff29bf91735773d74b703a63379c95764a124d2b
tarikul islam <tarikulmd519@gmail.com>
2023-05-28 18:28:14 +0600

    fourth commit

--- app/Http/Controllers/Admin/ProtfolioController.php
+++ app/Http/Controllers/Admin/ProtfolioController.php
@@ -29,16 +29,36 @@ class ProtfolioController extends Controller
 
                   $data->colam2_top = Storage::put('gallary', $request->file('colam2_top'));
               }
-            // $data->colam2_bottom = Storage::put('gallary', $request->file('colam2_bottom'));
-            // $data->colam3_top = Storage::put('gallary', $request->file('colam3_top'));
-            // $data->colam3_bottom = Storage::put('gallary', $request->file('colam3_bottom'));
-            // $data->colam4_top = Storage::put('gallary', $request->file('colam4_top'));
-            // $data->colam4_bottom = Storage::put('gallary', $request->file('colam4_bottom'));
+              if(request()->hasFile('colam2_bottom')){
+
+                   $data->colam2_bottom = Storage::put('gallary', $request->file('colam2_bottom'));
+              }
+              if(request()->hasFile('colam3_top')){
+
+                   $data->colam3_top = Storage::put('gallary', $request->file('colam3_top'));
+              }
+
+              if(request()->hasFile('colam3_bottom')){
+
+                   $data->colam3_bottom = Storage::put('gallary', $request->file('colam3_bottom'));
+              }
+              if(request()->hasFile('colam4_top')){
+
+                   $data->colam4_top = Storage::put('gallary', $request->file('colam4_top'));
+              }
+
+              if(request()->hasFile('colam4_bottom')){
+                     $data->colam4_bottom = Storage::put('gallary', $request->file('colam4_bottom'));
+              }
             $data->save(); 
       
-        
+           return redirect()->route('dashboard.protfolio.view');
+    }
 
 
-        // return redirect()->route('dashboard.about.view');
+    public function view(){
+        $all_data = Protfolio::get();
+        return view('admin.potfolio.view',compact('all_data'));
     }
 }
+
